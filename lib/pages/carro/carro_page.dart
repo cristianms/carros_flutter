b8ac68eeff97ef2732d867f23a4d69f166c929ba
Cristian <cristian@rech.com.br>
2020-01-27 12:45:16 -0300

    Aula 136

--- lib/pages/carro/carro_page.dart
+++ lib/pages/carro/carro_page.dart
@@ -2,7 +2,6 @@ import 'package:carros/pages/carro/carro.dart';
 import 'package:flutter/material.dart';
 
 class CarroPage extends StatelessWidget {
-
   Carro carro;
 
   CarroPage(this.carro);
@@ -25,10 +24,18 @@ class CarroPage extends StatelessWidget {
             onSelected: _onClickPopupMenu,
             itemBuilder: (BuildContext context) {
               return [
-                PopupMenuItem(value: "Editar", child: Text("Editar"),),
-                PopupMenuItem(value: "Deletar", child: Text("Deletar"),),
                 PopupMenuItem(
-                  value: "Compartilhar", child: Text("Compartilhar"),),
+                  value: "Editar",
+                  child: Text("Editar"),
+                ),
+                PopupMenuItem(
+                  value: "Deletar",
+                  child: Text("Deletar"),
+                ),
+                PopupMenuItem(
+                  value: "Compartilhar",
+                  child: Text("Compartilhar"),
+                ),
               ];
             },
           )
@@ -55,13 +62,45 @@ class CarroPage extends StatelessWidget {
   _body() {
     return Container(
         padding: EdgeInsets.all(16),
-        child: Image.network(carro.urlFoto)
-    );
+        child: ListView(
+          children: <Widget>[
+            Image.network(carro.urlFoto),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      carro.nome,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      carro.tipo,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.favorite), onPressed: onClickFavorito),
+                    IconButton(
+                        icon: Icon(Icons.share), onPressed: onClickShare),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ));
   }
 
-  void _onClickMapa() {
-  }
+  void _onClickMapa() {}
 
-  void _onClickVideo() {
-  }
+  void _onClickVideo() {}
+
+  void onClickFavorito() {}
+
+  void onClickShare() {}
 }
